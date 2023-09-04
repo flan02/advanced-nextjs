d1d39d5cbd3616c1ab3c5799e5a9048f1d132f7c
flan02 <chanivetdan@hotmail.com>
2023-09-04 19:21:31 -0300

    Creamos las consultas para el CRUD ya podemos hacer las operaciones y manejamos los errores con el cliente de Prisma. Seguimos con LISTAR NOTAS.

--- src/app/api/notes/[id]/route.ts
+++ src/app/api/notes/[id]/route.ts
@@ -1,19 +1,92 @@
 import { NextResponse } from "next/server";
+import { Prisma } from "@prisma/client";
+import { prisma } from "@/libs/prisma";
 
-export function GET() {
-    return NextResponse.json({
-        message: 'getting single note...'
-    })
+interface Params {
+    params:
+    {
+        id: string
+    }
 }
 
-export function DELETE() {
-    return NextResponse.json({
-        message: 'deleting single note...'
-    })
+export async function GET(req: Request, { params }: Params) {
+    try {
+        //throw new Error('artificial Error...')
+        const noteFound = await prisma.note.findUnique({ // findUnique busca un registro por su id tambien existe findFirst
+            where: {
+                id: Number(params.id) // el id es un string, pero en la bd es un int
+            }
+        })
+        //if (!noteFound) throw new Error(`Note ${params.id} not found`)
+        if (!noteFound) return NextResponse.json({
+            message: `Note ${params.id} not found`
+        }, { status: 404 })
+        return NextResponse.json(noteFound)
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            return NextResponse.json({
+                message: error.message
+            }, { status: 500 })
+        }
+    }
 }
 
-export function PUT() {
-    return NextResponse.json({
-        message: 'updating single note...'
-    })
+export async function DELETE(req: Request, { params }: Params) {
+    try {
+        /*
+        const noteFound = await prisma.note.findUnique({
+            where: {
+                id: Number(params.id)
+            }
+        })
+            if (!noteFound) {
+                return NextResponse.json({
+                    message: `Note ${params.id} not found`
+                }, { status: 404 })
+            } else {*/
+        const noteDeleted = await prisma.note.delete({
+            where: {
+                id: Number(params.id)
+            }
+        })
+        return NextResponse.json(`Note: ${noteDeleted.title} deleted`)
+        //  }   Validacion casera requiere mas codigo y mas consultas a la bd
+    } catch (error: unknown) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === 'P2025')
+                return NextResponse.json({
+                    message: `Note ${params.id} not found`
+                }, { status: 404 })
+        }
+        if (error instanceof Error) {
+            return NextResponse.json({
+                message: error.message
+            }, { status: 500 })
+        }
+    }
+}
+
+export async function PUT(req: Request, { params }: Params) {
+    try {
+        const data = await req.json()
+        const noteUpdated = await prisma.note.update({
+            where: {
+                id: Number(params.id)
+            },
+            data
+        })
+        return NextResponse.json(noteUpdated)
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === 'P2025')
+                return NextResponse.json({
+                    message: `Note ${params.id} not found`
+                }, { status: 404 })
+        }
+        if (error instanceof Error) {
+            return NextResponse.json({
+                message: error.message
+            }, { status: 500 })
+        }
+    }
 }
--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -1,13 +1,37 @@
 import { NextResponse } from "next/server"
+import { prisma } from "@/libs/prisma"
 
-export function GET() {
-    return NextResponse.json({
-        message: 'getting all notes...'
-    })
+export async function GET() {
+    try {
+        //throw new Error('artificial Error...')
+        const notesFound = await prisma.note.findMany()
+        return NextResponse.json(notesFound)
+    } catch (error: unknown) {
+        if (error instanceof Error) { // lo hizo el copiloto de github
+            return NextResponse.json({
+                message: error.message
+            }, { status: 500 })
+        }
+    }
+    throw new Error('Unreachable code detected.')
 }
 
-export function POST() {
-    return NextResponse.json({
-        message: 'creating note...'
-    })
+export async function POST(req: Request) { // Request objeto global de js que nos permite acceder a la info de la peticion
+    try {
+        //throw new Error('artificial Error...')
+        const { title, content } = await req.json() // son las prop enviadas por el cliente
+        const noteCreated = await prisma.note.create({
+            data: {
+                title,
+                content
+            }
+        })
+        return NextResponse.json(noteCreated)
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            return NextResponse.json({
+                message: error.message
+            }, { status: 500 })
+        }
+    }
 }
